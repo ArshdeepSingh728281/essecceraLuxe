0f6cdaf89c4f9df4d8b2865e595725ff69de65ef
Arshdeep728281 <ARSHDEEPSINGH728281@GMAIL.COM>
2025-05-10 10:05:36 +0530

    scndcommit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { usePageStore , Pages} from "../store/pageStore";
 import styles from "../styles/style.module.css";
 
