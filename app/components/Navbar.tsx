3861ca945386d17b58edd499b92c7ee7c736b1c5
Arshdeep728281 <ARSHDEEPSINGH728281@GMAIL.COM>
2025-05-21 11:28:37 +0530

    addedmenu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePageStore , Pages} from "../store/pageStore";
+import { useState } from "react";
 // import styles from "../styles/style.module.css";
 
 
@@ -9,20 +10,39 @@ import { usePageStore , Pages} from "../store/pageStore";
 
 export default function Navbar(){
 
+    const [menustate,setmenustate] = useState(false);
+
 
 
     return (
-        <div className="flex justify-between items-center w-full h-16 bg-white px-14 pt-10">
-            <div className="flex justify-between items-center h-full w-1/3">
+        <div className="flex xl:justify-between items-center w-full  h-10 xl:h-16 bg-white px-14 xl:pt-10 pt-8">
+            <div className="hidden justify-between items-center h-full w-1/3 xl:flex"> 
                 <NavbarItem to="about" text="About Us" />
                 <NavbarItem to="/" text="Home"/>
                 <NavbarItem to="newsandevents" text="News & Events"/>
             </div>
-            <img  src="mainlogo.png" className={`w-60 ml-4 z-10 `}/> 
-            <div className="flex justify-between items-center h-full w-1/3">
+
+            <div onClick={()=>{setmenustate(true);console.log("hello world")}}  className="flex justify-center items-center h-[60px] w-[30px] xl:hidden z-10">
+                <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+                </svg>
+            </div>
+
+            <div className="w-full  justify-center ml-[-30px]  items-center h-full flex xl:hidden">
+            <img  src="mainlogo.png" className={`xl:w-60 ml-4 z-10 w-50 `}/>   
+            </div>
+
+            <img  src="mainlogo.png" className={`w-60 ml-4 z-10  hidden xl:block `}/>    
+
+            <div className="hidden justify-between items-center h-full w-1/3 xl:flex">
                 <NavbarItem minwidth="135px" to="ourbrands" text="Our Brands"/>
                 <NavbarItem to="investorrelations" text="Investors"/>
                 <NavbarItem to="careers" text="Careers"/>
+            </div>
+
+
+            <div className={`${menustate ? "translate-x-0" : "translate-x-[-100%]"} transform transition-all fixed top-0 left-0 bottom-0 bg-black opacity-50 h-full w-[40%] z-20`}> 
+            {/* content */}
             </div>
 
         </div>
--- app/pages/main.tsx
+++ app/pages/main.tsx
@@ -37,6 +37,7 @@ export default function Main() {
           </div>
         </div>
       </div>
+      
 
 
 
