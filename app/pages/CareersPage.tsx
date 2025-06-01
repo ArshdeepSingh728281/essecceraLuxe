dc3ddf838d63463d4e22441b4f130433891df66f
Arshdeep728281 <ARSHDEEPSINGH728281@GMAIL.COM>
2025-06-01 22:14:14 +0530

    onlyaboutusleftokaddtran

--- app/pages/CareersPage.tsx
+++ app/pages/CareersPage.tsx
@@ -38,8 +38,8 @@ export default function AboutPage() {
 
   <div className="pointer-events-none absolute inset-y-0 right-5 flex items-center text-black">
 
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
 </svg>
 
   </div>
@@ -77,7 +77,7 @@ export default function AboutPage() {
 
 
 
-        <div className="mt-6 w-[41%] text-[18px] text-center bg-[#D4C9BE] font-[900] rounded-[5px] px-[20px] py-[16px] tran cursor-pointer hover:bg-[#b8a898] active:bg-[#887b6f] active:translate-y-1 select-none mb-[130px]">
+        <div className="tran mt-6 w-[41%] text-[18px] text-center bg-[#D4C9BE] font-[900] rounded-[5px] px-[20px] py-[16px]  cursor-pointer hover:bg-[#b8a898] active:bg-[#887b6f] active:translate-y-1 select-none mb-[130px]">
             Sumbit
         </div>
 
